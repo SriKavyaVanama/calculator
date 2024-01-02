ffc1faeae243205fdcfb15a16b3b8a1d70fb82fb
SriKavyaVanama <153215717+SriKavyaVanama@users.noreply.github.com>
2024-01-02 17:12:43 -0600

    code comments fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
